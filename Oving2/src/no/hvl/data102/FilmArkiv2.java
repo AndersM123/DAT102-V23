698627ab43893516fbf3f489c02cdf0ecd5b0bdf
Anders-Marius Gjerdalen <anders-marius@158-37-235-109.hvl.no>
2023-02-07 19:14:58 +0100

    kommet igang med øving3

---
+++ Oving2/src/no/hvl/data102/FilmArkiv2.java
@@ -0,0 +1,68 @@
+package no.hvl.data102;
+
+import no.hvl.data102.adt.FilmarkivADT;
+
+public class FilmArkiv2 implements FilmarkivADT {
+
+	private int antall;
+	private LinearNode<Film> start;
+	// Ingen referanse til siste, kun start
+	
+	public FilmArkiv2() {
+		start = null;
+		antall = 0;
+	}
+	
+	@Override
+	public Film finnFilm(int nr) {
+		Film film = null;
+		LinearNode<Film> aktuell = start;
+		
+		while(aktuell.getElement() != null) {
+			if(aktuell.getElement().getFilmnr() == nr) {
+				film = aktuell.getElement();
+			} else {
+				aktuell = aktuell.getNeste();	
+			}
+		}
+		return film;
+	}
+
+	@Override
+	public void leggTilFilm(Film nyFilm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean slettFilm(int filmnr) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Film[] soekTittel(String delstreng) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Film[] soekProdusent(String delstreng) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int antall(Sjanger sjanger) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int antall() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
